be622e74c4764432009a58b98f35c6e2d3d19c32
Haamid Raza Kazi <haamidraza03@gmail.com>
2024-04-19 01:05:18 +0530

    last change

--- api/index.js
+++ api/index.js
@@ -21,11 +21,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 const __dirname = path.resolve();
 
  
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
 
   
 
