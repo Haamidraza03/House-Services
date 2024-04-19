f887ac8931ff29cf71ee036007a0e8ac5eddff33
Haamid Raza Kazi <haamidraza03@gmail.com>
2024-04-19 07:59:34 +0530

    hosting

--- api/index.js
+++ api/index.js
@@ -30,14 +30,14 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/sp", spRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Welcome");
-});
-
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.get("/", (req, res) => {
+    res.send("Welcome");
 });
 
 
