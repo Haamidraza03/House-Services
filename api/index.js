0b70b52d2fc2169a60dcf25571bd2d63270cda05
Sakshi2601 <sakshichoudaryg26@gmail.com>
2024-04-19 11:47:26 +0530

    comment

--- api/index.js
+++ api/index.js
@@ -34,11 +34,11 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/sp", spRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
 
 
 app.use((err, req, res, next) => {
